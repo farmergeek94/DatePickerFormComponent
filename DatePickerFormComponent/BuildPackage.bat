b0a944ca4d17b863c7ae5369d217784249ee47f4
Joshua Hess <jhess@hbs.net>
2019-08-21 15:28:49 -0500

    Final chnages before publish

--- DatePickerFormComponent/BuildPackage.bat
+++ DatePickerFormComponent/BuildPackage.bat
@@ -1,3 +1,3 @@
-nuget pack DatePickerFormComponent.csproj -Prop Configuration=Release
+C:\Users\jhess\Documents\Customers\Kentico\Projects\nuget.exe pack DatePickerFormComponent.csproj -Prop Configuration=Release
 @echo off
 pause
--- DatePickerFormComponent/Content/FormComponents/DatePickerComponent/DatePickerComponent.js
+++ DatePickerFormComponent/Content/FormComponents/DatePickerComponent/DatePickerComponent.js
@@ -1,31 +1,21 @@
-﻿(function ($) {
-    var self = this;
-    this.loadFunction = function () {
-        // Check for jQuery
-        if (window.jQuery) {
-            // Check for the datepicker.  If not found load manually.
-            if (window.jQuery.datetimepicker) {
-                $(".datepicker").datetimepicker();
-            } else {
-                $.ajaxSetup({
-                    cache: true
-                });
-                $.getScript("/Content/FormComponents/DatePickerComponent/datetimepicker/jquery.datetimepicker.js").done(function (script, textStatus) {
-                    $(".datepicker").datetimepicker();
-                });
-            }
+﻿function createDatePickerFormComponent(formControl) {
+    var htmlContentInput = document.querySelector("[name='" + formControl + "'");
+
+    // Check for jQuery
+    if (window.jQuery) {
+        // Check for the datepicker.  If not found load manually.
+        if (window.jQuery.datetimepicker) {
+            $(htmlContentInput).datetimepicker();
         } else {
-            console.log("jQuery not found. Waiting for load 2 seconds.");
-            setTimeout(2000, this.loadFunction);
+            $.ajaxSetup({
+                cache: true
+            });
+            $.getScript("/Content/FormComponents/DatePickerComponent/datetimepicker/jquery.datetimepicker.js").done(function (script, textStatus) {
+                $(htmlContentInput).datetimepicker();
+            });
         }
-    };
-    if (document.readyState === "loading") {
-        // Calls the function during the 'DOMContentLoaded' event, after the HTML document has been completely loaded
-        document.addEventListener("DOMContentLoaded", function () {
-            self.loadFunction();
-        });
     } else {
-        // Calls the function directly in cases where the component is rendered dynamically after 'DOMContentLoaded' has occurred
-        self.loadFunction();
+        console.log("jQuery not found. Waiting for load 2 seconds.");
+        setTimeout(2000, createDatePickerFormComponent(formControl));
     }
-})(jQuery);
+}
