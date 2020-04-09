a69c9c2ef326a686a9ae3727f8d8f1078176c602
farmergeek94 <51802990+farmergeek94@users.noreply.github.com>
2020-04-09 11:06:29 -0500

    Updated to add options
    This is in preparation for allowing you to set a format within kentico.

--- DatePickerFormComponent/Content/FormComponents/DatePickerComponent/DatePickerComponent.js
+++ DatePickerFormComponent/Content/FormComponents/DatePickerComponent/DatePickerComponent.js
@@ -1,17 +1,21 @@
-﻿function createDatePickerFormComponent(formControl) {
+function createDatePickerFormComponent(formControl) {
     var htmlContentInput = document.querySelector("[name='" + formControl + "'");
-
+    var datetimepickerOptions = {
+        format: 'm/d/Y H:i:s',
+        formatDate: 'm/d/Y',
+        formatTime: "H:i"
+    };
     // Check for jQuery
     if (window.jQuery) {
         // Check for the datepicker.  If not found load manually.
         if (window.jQuery.datetimepicker) {
-            $(htmlContentInput).datetimepicker();
+            $(htmlContentInput).datetimepicker(datetimepickerOptions);
         } else {
             $.ajaxSetup({
                 cache: true
             });
             $.getScript("/Content/FormComponents/DatePickerComponent/datetimepicker/jquery.datetimepicker.js").done(function (script, textStatus) {
-                $(htmlContentInput).datetimepicker();
+                $(htmlContentInput).datetimepicker(datetimepickerOptions);
             });
         }
     } else {
